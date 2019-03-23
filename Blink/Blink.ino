c0b97332f066b567fee85d72a63df759ca69b343
zakrai <34668215+zakrai@users.noreply.github.com>
2019-03-23 20:56:07 +0700

    add func

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,12 +1,8 @@
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  led_setup();
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  led_blink();
+  while(!false);
 }
---
+++ Blink/led.ino
@@ -0,0 +1,12 @@
+#define led 2
+
+void led_setup(){
+  pinMode(led, OUTPUT);
+}
+
+void led_blink(){
+  for (int i=0; i<5; i++){
+    digitalWrite(led, HIGH);delay(500);
+    digitalWrite(led, LOW);delay(500);
+  }
+}
